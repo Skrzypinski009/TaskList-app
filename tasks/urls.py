fdc8f62bebe8aa58bae53d0ec1e011c7966a5a89
Marek Skrzypiński <skrzypinski009@gmail.com>
2026-03-10 13:48:10 +0200

    urls changes

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,16 +5,19 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="home"),
+    # AUTH
     path("register", views.register, name="register"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("thanks", views.thanks_register, name="thanks_register"),
+    # TASK LIST
     path("task_list/<int:task_id>", views.task_list, name="task_list"),
-    path("add_list", views.add_list, name="add_list"),
-    path("edit_list/<int:list_id>", views.edit_list, name="edit_list"),
-    path("delete_list/<int:list_id>", views.delete_list, name="delete_list"),
+    path("task_list/add", views.add_list, name="add_list"),
+    path("task_list/<int:list_id>/edit", views.edit_list, name="edit_list"),
+    path("task_list/<int:list_id>/delete", views.delete_list, name="delete_list"),
+    path("tasklist/<int:list_id>/add_task", views.add_task, name="add_task"),
+    # TASK
     path("task/<int:task_id>/toggle", views.toggle_task, name="task_toggle"),
-    path("add_task/<int:list_id>", views.add_task, name="add_task"),
-    path("delete_task/<int:task_id>", views.delete_task, name="delete_task"),
-    path("edit_task/<int:task_id>", views.edit_task, name="edit_task"),
+    path("task/<int:task_id>/delete", views.delete_task, name="delete_task"),
+    path("task/<int:task_id>/edit", views.edit_task, name="edit_task"),
 ]
