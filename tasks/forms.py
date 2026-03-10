cc48977a4514e49e844272c7772370f14cc7e80d
Marek Skrzypiński <skrzypinski009@gmail.com>
2026-03-10 19:26:33 +0200

    views update

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,21 +1,62 @@
 from django import forms
 from .models import Task, TaskList
 
+
 class RegisterForm(forms.Form):
-    username = forms.CharField(label='Username', max_length=100, required=True)
-    password = forms.CharField(label='Password', max_length=100, required=True, widget=forms.PasswordInput)
-    email = forms.EmailField(label='Email', required=True)
+    username = forms.CharField(
+        label="Username",
+        max_length=100,
+        required=True,
+        widget=forms.TextInput(attrs={"class": "form-control"}),
+    )
+    password = forms.CharField(
+        label="Password",
+        max_length=100,
+        required=True,
+        widget=forms.PasswordInput(attrs={"class": "form-control"}),
+    )
+    email = forms.EmailField(
+        label="Email",
+        required=True,
+        widget=forms.TextInput(attrs={"class": "form-control"}),
+    )
+
 
 class LoginForm(forms.Form):
-    username = forms.CharField(label='Username', max_length=100, required=True)
-    password = forms.CharField(label='Password', max_length=100, required=True, widget=forms.PasswordInput)
+    username = forms.CharField(
+        label="Username",
+        max_length=100,
+        required=True,
+        widget=forms.TextInput(attrs={"class": "form-control"}),
+    )
+    password = forms.CharField(
+        label="Password",
+        max_length=100,
+        required=True,
+        widget=forms.PasswordInput(attrs={"class": "form-control"}),
+    )
+
 
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ['title', 'description', 'deadline']
+        fields = ["title", "description", "deadline"]
+        widgets = {
+            "title": forms.TextInput(attrs={"class": "form-control"}),
+            "description": forms.Textarea(attrs={"class": "form-control", "rows": 4}),
+            "deadline": forms.DateInput(
+                attrs={
+                    "type": "date",
+                    "class": "form-control",
+                }
+            ),
+        }
+
 
 class TaskListForm(forms.ModelForm):
     class Meta:
         model = TaskList
-        fields = ['title']
+        fields = ["title"]
+        widgets = {
+            "title": forms.TextInput(attrs={"class": "form-control"}),
+        }
